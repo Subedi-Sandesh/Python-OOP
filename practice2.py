7f616d834f6dbe89d9d43dad804ff6cb74190b09
Sandesh Subedi <151416315+Subedi-Sandesh@users.noreply.github.com>
2024-07-21 07:55:32 +0545

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -7,13 +7,13 @@ class Account:
     def debit(self, amount):
         self.balance -= amount
         print(f"Your {self.acc_no} has been Debited by NPR{amount}\nYour new balance is {self.balance}")
-        
+
     #Credit method
     def credit(self, amount):
         self.balance += amount
         print(f"Your {self.acc_no} has been Credited by NPR{amount}\nYour new balance is {self.balance}")
   
-acc1 = Account(10000, 12345)
+acc1 = Account(10000, 12345)  #creating instance
 
 acc1.debit(1000)
 acc1.credit(12000)
